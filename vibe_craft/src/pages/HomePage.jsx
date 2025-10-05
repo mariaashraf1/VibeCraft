748bffc7766bd4fb463d06b95a20df4a34ee7056
Maria ashraf <mariaashraf202@gmail.com>
2025-10-05 23:30:57 +0300

    Added Footer and start styling home page

--- vibe_craft/src/pages/HomePage.jsx
+++ vibe_craft/src/pages/HomePage.jsx
@@ -1,27 +1,41 @@
 import home2 from "../assets/home2.png";
 import home1 from "../assets/home1.png";
+import girl6 from "../assets/girl6.png";
 export default function HomePage() {
     return (
-    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         {/* start of main container */}
-      <div className=" w-[95%] mt-20 mx-6 bg-[#2C2C3C] rounded-lg min-h-screen overflow-auto shadow-lg">
-        <div className="flex flex-col "> {/* start of  main col */}
-        <div className="flex items-start justify-between mt-20 "> {/* start of row of pic and text */}
-        <div className="flex flex-col "> 
-        <div className="flex flex-col items-start justify-center ml-20 mt-20 "> {/* start of text */}
-        <h1 className="text-5xl font-bold flex justify-start ">
-            Connect Your Mood with </h1>
-            <h1 className="text-5xl font-semibold flex justify-start mt-6">
-            Music & Art!</h1>
-             </div>
-             <h3 className="text-2xl  flex justify-start ml-20 mt-10 ">
-            Describe Your Mood to get started
-        </h3>
-        <button className="bg-[#7A9CAB] text-white px-4 py-2 rounded-full mt-10 ml-20">"Start Now"</button>
-        </div>
-        <img src={home2} alt="home_page"  className="w-[200px] md:w-[400px] lg:w-[600px] h-auto  object-contain" />
-        </div>
-        </div>
+        <div className=" w-[95%] mt-20 mx-6 bg-[#2C2C3C] rounded-lg min-h-screen overflow-auto shadow-lg">
+          <div className="flex flex-col ">{/* start of  main col */}
+            <div className="flex items-center justify-between mt-20 ">  {/* start of row of pic and text */}
+              <div className="flex flex-col ">
+                <div className="flex flex-col items-start justify-center ml-20 mt-10 ">
+                  {/* start of text */}
+                  <h1 className="text-5xl font-bold italic ">
+                    Connect Your Mood with
+                  </h1>
+                  <h1 className="text-5xl font-semibold italic mt-6">
+                    Music & Art!
+                  </h1>
+                </div>
+                <h3 className="text-2xl  ml-20 mt-10 ">
+                  Describe Your Mood to get started
+                </h3>
+                <button className="bg-[#7A9CAB] text-white font-semibold py-2 rounded-full ml-20 mt-10 w-32 sm:w-40 md:w-48 hover:bg-[#6a8f9b] transition">
+                  Start Now
+                </button>
+              </div>
+                <div className="relative mr-20">
+                  <div className="absolute inset-0 bg-gradient-to-r from-[#7A9CAB] to-[#2C2C3C] rounded-full blur-3xl opacity-30 scale-110"></div>
+                  <div className="absolute inset-0 bg-gradient-to-r from-[#7A9CAB] to-[#2C2C3C] rounded-full blur-2xl opacity-40 scale-105"></div>
+                  <img
+                   src={girl6}
+                   alt="home_page"
+                   className="relative w-[200px] md:w-[400px] lg:w-[400px] h-auto object-contain z-10"
+                  />
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     );
